9c6ef21fbcb654a09c6af9aa6866f3ecfb151871
据说甜蜜呢 <lijun@geekbuy.com.cn>
2018-11-24 16:08:25 +0800

    添加类文件

---
+++ Foundatio.ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Foundatio.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
