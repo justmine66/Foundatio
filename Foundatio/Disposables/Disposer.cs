e555759f8257bf3574130af3e97609e26e3efaab
据说甜蜜呢 <lijun@geekbuy.com.cn>
2019-01-18 11:40:23 +0800

    添加文件

---
+++ Foundatio/Disposables/Disposer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Foundatio.Disposables
+{
+    public abstract class Disposer : IDisposable
+    {
+        private bool _disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected abstract void FreeManagedResources();
+        protected abstract void FreeUnManagedResources();
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return; ;
+
+            if (disposing)
+            {
+                // Free managed resources here.
+                FreeManagedResources();
+            }
+
+            // Free unmanaged resources here.
+            FreeUnManagedResources();
+
+            _disposed = true;
+        }
+
+        ~Disposer()
+        {
+            Dispose(false);
+        }
+    }
+}
